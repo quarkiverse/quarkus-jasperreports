0bff1919d7d4bce5ea2528a73e1aba1db06721d8
Melloware <mellowaredev@gmail.com>
2024-10-02 07:55:08 -0400

    Initial code cleanup, Quarkus 3.15 LTS, fixed versions, Dev UI (#57)
    * Initial code cleanup, Quarkus 3.15 LTS, fixed versions, Dev UI
    
    * Update deployment/src/main/java/io/quarkiverse/jasperreports/deployment/devui/JasperReportsDevUIProcessor.java
    
    Co-authored-by: George Gastaldi <gegastaldi@gmail.com>
    
    ---------
    
    Co-authored-by: George Gastaldi <gegastaldi@gmail.com>

--- deployment/src/main/java/io/quarkiverse/jasperreports/deployment/JasperreportsProcessor.java
+++ deployment/src/main/java/io/quarkiverse/jasperreports/deployment/JasperreportsProcessor.java
@@ -3,15 +3,24 @@ package io.quarkiverse.jasperreports.deployment;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBundleBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
+import io.quarkus.deployment.pkg.builditem.UberJarMergedResourceBuildItem;
 
 class JasperreportsProcessor {
 
     private static final String FEATURE = "jasperreports";
+    private static final String EXTENSIONS_FILE = "jasperreports_extension.properties";
 
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    UberJarMergedResourceBuildItem mergeResource() {
+        return new UberJarMergedResourceBuildItem(EXTENSIONS_FILE);
     }
 
     @BuildStep
@@ -20,5 +29,21 @@ class JasperreportsProcessor {
         // see also https://github.com/quarkusio/quarkus/issues/31224
         producer.produce(new RuntimeInitializedClassBuildItem("net.sf.jasperreports.engine.SimpleReportContext"));
         producer.produce(new RuntimeInitializedClassBuildItem("net.sf.jasperreports.engine.design.JRAbstractCompiler"));
+    }
+
+    @BuildStep
+    void substrateResourceBuildItems(BuildProducer<NativeImageResourceBuildItem> nativeImageResourceProducer,
+            BuildProducer<NativeImageResourceBundleBuildItem> resourceBundleBuildItem) {
+        nativeImageResourceProducer.produce(new NativeImageResourceBuildItem(
+                EXTENSIONS_FILE,
+                "default.jasperreports.properties",
+                "jasperreports_messages.properties",
+                "metadata_messages.properties",
+                "metadata_messages-defaults.properties",
+                "properties-metadata.json"));
+
+        resourceBundleBuildItem.produce(new NativeImageResourceBundleBuildItem("jasperreports_messages"));
+        resourceBundleBuildItem.produce(new NativeImageResourceBundleBuildItem("metadata_messages"));
+        resourceBundleBuildItem.produce(new NativeImageResourceBundleBuildItem("metadata_messages-defaults"));
     }
 }
---
+++ deployment/src/main/java/io/quarkiverse/jasperreports/deployment/devui/JasperReportsDevUIProcessor.java
@@ -0,0 +1,29 @@
+package io.quarkiverse.jasperreports.deployment.devui;
+
+import io.quarkus.deployment.IsDevelopment;
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.devui.spi.page.CardPageBuildItem;
+import io.quarkus.devui.spi.page.ExternalPageBuilder;
+import io.quarkus.devui.spi.page.Page;
+import net.sf.jasperreports.engine.JasperReport;
+
+public class JasperReportsDevUIProcessor {
+
+    @BuildStep(onlyIf = IsDevelopment.class)
+    void createVersion(BuildProducer<CardPageBuildItem> cardPageBuildItemBuildProducer) {
+        final CardPageBuildItem card = new CardPageBuildItem();
+
+        final ExternalPageBuilder versionPage = Page.externalPageBuilder("JasperReports Version")
+                .icon("font-awesome-regular:tag")
+                .url("https://community.jaspersoft.com/download-jaspersoft/community-edition/")
+                .doNotEmbed()
+                .staticLabel(JasperReport.class.getPackage().getSpecificationVersion());
+
+        card.addPage(versionPage);
+
+        card.setCustomCard("qwc-jasperreports-card.js");
+
+        cardPageBuildItemBuildProducer.produce(card);
+    }
+}
