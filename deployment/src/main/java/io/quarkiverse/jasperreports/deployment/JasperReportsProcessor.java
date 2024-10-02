6862f991756770ada2e96c06fad0d0ad28dcab3b
melloware <mellowaredev@gmail.com>
2024-10-02 09:13:14 -0400

    Native: net.sf.jasperreports.engine.util.json.DefaultJsonQLExecuter

--- deployment/src/main/java/io/quarkiverse/jasperreports/deployment/JasperReportsProcessor.java
+++ deployment/src/main/java/io/quarkiverse/jasperreports/deployment/JasperReportsProcessor.java
@@ -32,7 +32,8 @@ class JasperReportsProcessor {
                 net.sf.jasperreports.engine.SimpleReportContext.class.getName(),
                 net.sf.jasperreports.engine.design.JRAbstractCompiler.class.getName(),
                 net.sf.jasperreports.renderers.AbstractSvgDataToGraphics2DRenderer.class.getName(),
-                net.sf.jasperreports.renderers.util.SvgFontProcessor.class.getName()
+                net.sf.jasperreports.renderers.util.SvgFontProcessor.class.getName(),
+                net.sf.jasperreports.engine.util.json.DefaultJsonQLExecuter.class.getName()
                 )
                 .map(RuntimeInitializedPackageBuildItem::new)
                 .forEach(runtimeInitializedPackages::produce);
