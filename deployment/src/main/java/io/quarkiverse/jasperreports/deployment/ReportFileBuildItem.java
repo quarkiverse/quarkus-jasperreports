7af6139961fdd03d5e0340c05e804e5462c48e07
Melloware <mellowaredev@gmail.com>
2024-10-04 18:39:18 -0400

    DevUI: add report files (#95)

---
+++ deployment/src/main/java/io/quarkiverse/jasperreports/deployment/ReportFileBuildItem.java
@@ -0,0 +1,49 @@
+package io.quarkiverse.jasperreports.deployment;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.commons.io.FilenameUtils;
+
+import io.quarkus.builder.item.MultiBuildItem;
+
+/**
+ * This build item represents a single watched Report file either .jrxml, .jasper, or .jrtx
+ */
+public final class ReportFileBuildItem extends MultiBuildItem {
+
+    public final static String EXT_REPORT = "jrxml";
+    public final static String EXT_STYLE = "jrtx";
+    public final static String EXT_COMPILED = "jasper";
+    public final static List<String> EXTENSIONS = List.of("." + EXT_REPORT, "." + EXT_COMPILED, "." + EXT_STYLE);
+
+    private final Path path;
+
+    public ReportFileBuildItem(Path path) {
+        this.path = path;
+    }
+
+    public Path getPath() {
+        return path;
+    }
+
+    public String getFileName() {
+        return path.getFileName().toString();
+    }
+
+    public String getParent() {
+        return path.getParent().toString();
+    }
+
+    public String getType() {
+        String extension = FilenameUtils.getExtension(path.getFileName().toString()).toLowerCase(Locale.ROOT);
+        return switch (extension) {
+            case EXT_COMPILED -> "COMPILED";
+            case EXT_REPORT -> "REPORT";
+            case EXT_STYLE -> "STYLES";
+            default -> "???";
+        };
+    }
+
+}
--- deployment/src/main/java/io/quarkiverse/jasperreports/deployment/devui/JasperReportsDevUIProcessor.java
+++ deployment/src/main/java/io/quarkiverse/jasperreports/deployment/devui/JasperReportsDevUIProcessor.java
@@ -1,17 +1,23 @@
 package io.quarkiverse.jasperreports.deployment.devui;
 
+import java.util.List;
+
+import io.quarkiverse.jasperreports.deployment.ReportFileBuildItem;
 import io.quarkus.deployment.IsDevelopment;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.devui.spi.page.CardPageBuildItem;
 import io.quarkus.devui.spi.page.ExternalPageBuilder;
 import io.quarkus.devui.spi.page.Page;
+import io.quarkus.devui.spi.page.PageBuilder;
+import io.quarkus.devui.spi.page.TableDataPageBuilder;
 import net.sf.jasperreports.engine.JasperReport;
 
 public class JasperReportsDevUIProcessor {
 
     @BuildStep(onlyIf = IsDevelopment.class)
-    void createVersion(BuildProducer<CardPageBuildItem> cardPageBuildItemBuildProducer) {
+    void createVersion(BuildProducer<CardPageBuildItem> cardPageBuildItemBuildProducer,
+            List<ReportFileBuildItem> reportFiles) {
         final CardPageBuildItem card = new CardPageBuildItem();
 
         final ExternalPageBuilder versionPage = Page.externalPageBuilder("JasperReports Version")
@@ -21,6 +27,18 @@ public class JasperReportsDevUIProcessor {
                 .staticLabel(JasperReport.class.getPackage().getSpecificationVersion());
 
         card.addPage(versionPage);
+
+        final PageBuilder<TableDataPageBuilder> reportsPage = Page.tableDataPageBuilder("Reports")
+                .icon("font-awesome-solid:file-pdf")
+                .staticLabel(String.valueOf(reportFiles.size()))
+                .showColumn("fileName")
+                .showColumn("type")
+                .showColumn("parent")
+                .buildTimeDataKey("reports");
+
+        card.addPage(reportsPage);
+
+        card.addBuildTimeData("reports", reportFiles);
 
         card.setCustomCard("qwc-jasperreports-card.js");
 
