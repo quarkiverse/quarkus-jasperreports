f051f77daa5ce5117d9c096ca3dc60c6fb0bc95a
melloware <mellowaredev@gmail.com>
2024-10-18 11:55:20 -0400

    Fix #134: Hyperlink testing

---
+++ integration-tests/src/test/java/io/quarkiverse/jasperreports/it/JasperReportsHyperLinkResourceTest.java
@@ -0,0 +1,20 @@
+package io.quarkiverse.jasperreports.it;
+
+import static io.restassured.RestAssured.given;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+class JasperReportsHyperLinkResourceTest {
+
+    @Test
+    void testHyperlinks() {
+        given()
+                .when().get("/jasper/hyperlink/pdf")
+                .then()
+                .statusCode(200);
+    }
+
+}
